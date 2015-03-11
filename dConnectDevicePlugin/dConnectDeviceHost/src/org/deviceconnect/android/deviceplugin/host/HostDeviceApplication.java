b25a0bf3bd9093c71841ada7bad3e12474729466
畠山剛雄 <takeo@gclue.jp>
2015-03-11 21:08:15 +0900

    Add cache time management.

--- dConnectDevicePlugin/dConnectDeviceHost/src/org/deviceconnect/android/deviceplugin/host/HostDeviceApplication.java
+++ dConnectDevicePlugin/dConnectDeviceHost/src/org/deviceconnect/android/deviceplugin/host/HostDeviceApplication.java
@@ -24,22 +24,43 @@ public class HostDeviceApplication extends Application {
 
     /** Touch profile onTouch cache. */
     Bundle mOnTouchCache = null;
-    
+
+    /** Touch profile onTouch cache time. */
+    long mOnTouchCacheTime = 0;
+
     /** Touch profile onTouchStart cache. */
     Bundle mOnTouchStartCache = null;
-    
+
+    /** Touch profile onTouchStart cache time. */
+    long mOnTouchStartCacheTime = 0;
+
     /** Touch profile onTouchEnd cache. */
     Bundle mOnTouchEndCache = null;
-    
+
+    /** Touch profile onTouchEnd cache time. */
+    long mOnTouchEndCacheTime = 0;
+
     /** Touch profile onDoubleTap cache. */
     Bundle mOnDoubleTapCache = null;
-    
+
+    /** Touch profile onDoubleTap cache time. */
+    long mOnDoubleTapCacheTime = 0;
+
     /** Touch profile onTouchMove cache. */
     Bundle mOnTouchMoveCache = null;
-    
+
+    /** Touch profile onTouchMove cache time. */
+    long mOnTouchMoveCacheTime = 0;
+
     /** Touch profile onTouchCancel cache. */
     Bundle mOnTouchCancelCache = null;
-    
+
+    /** Touch profile onTouchCancel cache time. */
+    long mOnTouchCancelCacheTime = 0;
+
+    /** Touch profile cache retention time (mSec). */
+    static final long CACHE_RETENTION_TIME = 10000;
+
     /**
      * Get Touch cache data.
      * 
@@ -47,18 +68,43 @@ public class HostDeviceApplication extends Application {
      * @return Touch cache data.
      */
     public Bundle getTouchCache(final String attr) {
+        long lCurrentTime = System.currentTimeMillis();
         if (attr.equals(TouchProfile.ATTRIBUTE_ON_TOUCH)) {
-            return mOnTouchCache;
+            if (lCurrentTime - mOnTouchCacheTime <= CACHE_RETENTION_TIME) {
+                return mOnTouchCache;
+            } else {
+                return null;
+            }
         } else if (attr.equals(TouchProfile.ATTRIBUTE_ON_TOUCH_START)) {
-            return mOnTouchStartCache;
+            if (lCurrentTime - mOnTouchStartCacheTime <= CACHE_RETENTION_TIME) {
+                return mOnTouchStartCache;
+            } else {
+                return null;
+            }
         } else if (attr.equals(TouchProfile.ATTRIBUTE_ON_TOUCH_END)) {
-            return mOnTouchEndCache;
+            if (lCurrentTime - mOnTouchEndCacheTime <= CACHE_RETENTION_TIME) {
+                return mOnTouchEndCache;
+            } else {
+                return null;
+            }
         } else if (attr.equals(TouchProfile.ATTRIBUTE_ON_DOUBLE_TAP)) {
-            return mOnDoubleTapCache;
+            if (lCurrentTime - mOnDoubleTapCacheTime <= CACHE_RETENTION_TIME) {
+                return mOnDoubleTapCache;
+            } else {
+                return null;
+            }
         } else if (attr.equals(TouchProfile.ATTRIBUTE_ON_TOUCH_MOVE)) {
-            return mOnTouchMoveCache;
+            if (lCurrentTime - mOnTouchMoveCacheTime <= CACHE_RETENTION_TIME) {
+                return mOnTouchMoveCache;
+            } else {
+                return null;
+            }
         } else if (attr.equals(TouchProfile.ATTRIBUTE_ON_TOUCH_CANCEL)) {
-            return mOnTouchCancelCache;
+            if (lCurrentTime - mOnTouchCancelCacheTime <= CACHE_RETENTION_TIME) {
+                return mOnTouchCancelCache;
+            } else {
+                return null;
+            }
         } else {
             return null;
         }
@@ -71,18 +117,25 @@ public class HostDeviceApplication extends Application {
      * @param touchData Touch data.
      */
     public void setTouchCache(final String attr, final Bundle touchData) {
+        long lCurrentTime = System.currentTimeMillis();
         if (attr.equals(TouchProfile.ATTRIBUTE_ON_TOUCH)) {
             mOnTouchCache = touchData;
+            mOnTouchCacheTime = lCurrentTime;
         } else if (attr.equals(TouchProfile.ATTRIBUTE_ON_TOUCH_START)) {
             mOnTouchStartCache = touchData;
+            mOnTouchStartCacheTime = lCurrentTime;
         } else if (attr.equals(TouchProfile.ATTRIBUTE_ON_TOUCH_END)) {
             mOnTouchEndCache = touchData;
+            mOnTouchEndCacheTime = lCurrentTime;
         } else if (attr.equals(TouchProfile.ATTRIBUTE_ON_DOUBLE_TAP)) {
             mOnDoubleTapCache = touchData;
+            mOnDoubleTapCacheTime = lCurrentTime;
         } else if (attr.equals(TouchProfile.ATTRIBUTE_ON_TOUCH_MOVE)) {
             mOnTouchMoveCache = touchData;
+            mOnTouchMoveCacheTime = lCurrentTime;
         } else if (attr.equals(TouchProfile.ATTRIBUTE_ON_TOUCH_CANCEL)) {
             mOnTouchCancelCache = touchData;
+            mOnTouchCancelCacheTime = lCurrentTime;
         }
     }
 

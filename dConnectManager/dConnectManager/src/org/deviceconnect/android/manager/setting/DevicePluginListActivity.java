752784552d4ea477ffd30001a259184987c5b379
TakeoHatakeyama <takeo@gclue.jp>
2015-05-14 19:20:38 +0900

    Modify Manager.   Add device plug-in list fragment.

---
+++ dConnectManager/dConnectManager/src/org/deviceconnect/android/manager/setting/DevicePluginListActivity.java
@@ -0,0 +1,37 @@
+/*
+ DevicePluginListActivity.java
+ Copyright (c) 2015 NTT DOCOMO,INC.
+ Released under the MIT license
+ http://opensource.org/licenses/mit-license.php
+ */
+package org.deviceconnect.android.manager.setting;
+
+import org.deviceconnect.android.manager.R;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+
+/**
+ * Device Connect Manager device plug-in list Activity.
+ * 
+ * @author NTT DOCOMO, INC.
+ */
+public class DevicePluginListActivity extends Activity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.activity_devicepluginlist_title);
+        
+        if (savedInstanceState == null) {
+            Fragment f = new DevicePluginListFragment();
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction t = fm.beginTransaction();
+            t.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+            t.add(android.R.id.content, f, "continar");
+            t.commit();
+        }
+    }
+}

05d236e2e3eb2cf0eeb864866dedd09fdc2f5550
Shigekazu Fukui <fukui@gclue.jp>
2015-07-14 19:20:28 +0900

    Updated System profile. Device System Wakeup API was implemented.
    The API provides visualized setting tutorials for popular AllJoyn products. In this update, LIFX was added to the tutorials.

--- dConnectDevicePlugin/dConnectDeviceAllJoyn/app/src/main/java/org/deviceconnect/android/deviceplugin/alljoyn/activity/AllJoynSettingActivity.java
+++ dConnectDevicePlugin/dConnectDeviceAllJoyn/app/src/main/java/org/deviceconnect/android/deviceplugin/alljoyn/activity/AllJoynSettingActivity.java
@@ -1,22 +1,94 @@
 package org.deviceconnect.android.deviceplugin.alljoyn.activity;
 
-import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
-import org.deviceconnect.android.ui.activity.DConnectSettingPageFragmentActivity;
+import org.deviceconnect.android.deviceplugin.alljoyn.R;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 設定アクティビティー。
  *
  * @author NTT DOCOMO, INC.
  */
-public class AllJoynSettingActivity extends DConnectSettingPageFragmentActivity {
-    @Override
-    public int getPageCount() {
-        return 1;
-    }
+public class AllJoynSettingActivity extends Activity {
+
+    private static final int FIRST_PAGE_LAYOUT_INDEX = 1;
+    private static final List<SettingData> SETTING_LIST = Arrays.asList(
+            new SettingData(R.layout.aj_settings_lifx_list_item, AllJoynSettingLIFXActivity.class)
+    );
+
+    private LayoutInflater mInflator;
 
     @Override
-    public Fragment createPage(int i) {
-        return new Fragment();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aj_settings_master);
+
+        mInflator = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(new SettingListAdapter());
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                SettingData settingData = SETTING_LIST.get(position);
+                Intent intent = new Intent(AllJoynSettingActivity.this,
+                        settingData.detailActivityClass);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private class SettingListAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return SETTING_LIST.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView != null) {
+                Integer tagPosition = (Integer) convertView.getTag();
+                if (tagPosition == position) {
+                    return convertView;
+                }
+            }
+
+            SettingData settingData = SETTING_LIST.get(position);
+            View view = mInflator.inflate(settingData.listItemLayoutId, null);
+            view.setTag(position);
+            return view;
+        }
+    }
+
+    private static class SettingData {
+        public final int listItemLayoutId;
+        public final Class detailActivityClass;
+
+        public SettingData(int listItemLayoutId, Class detailActivityClass) {
+            this.listItemLayoutId = listItemLayoutId;
+            this.detailActivityClass = detailActivityClass;
+        }
     }
 }
---
+++ dConnectDevicePlugin/dConnectDeviceAllJoyn/app/src/main/java/org/deviceconnect/android/deviceplugin/alljoyn/activity/AllJoynSettingLIFXActivity.java
@@ -0,0 +1,36 @@
+package org.deviceconnect.android.deviceplugin.alljoyn.activity;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import org.deviceconnect.android.deviceplugin.alljoyn.R;
+
+/**
+ * 設定アクティビティー: LIFX
+ *
+ * @author NTT DOCOMO, INC.
+ */
+public class AllJoynSettingLIFXActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aj_settings_lifx_01);
+
+        findViewById(R.id.imageView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("market://details?id=com.lifx.lifx")));
+                } catch (ActivityNotFoundException ex) {
+                    // Simply launch Google Play instead.
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://")));
+                }
+            }
+        });
+    }
+}

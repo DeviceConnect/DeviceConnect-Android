6a1f2801b71b0f04ffdd46b0bbe8b149a2c7c547
Shigekazu Fukui <fukui@gclue.jp>
2015-08-21 17:28:05 +0900

    In Host device plugin, update Phone Call API.
    Permission CALL_PHONE is handled.

--- dConnectDevicePlugin/dConnectDeviceHost/src/org/deviceconnect/android/deviceplugin/host/profile/HostPhoneProfile.java
+++ dConnectDevicePlugin/dConnectDeviceHost/src/org/deviceconnect/android/deviceplugin/host/profile/HostPhoneProfile.java
@@ -6,9 +6,12 @@
  */
 package org.deviceconnect.android.deviceplugin.host.profile;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.deviceconnect.android.activity.PermissionRequestActivity;
 import org.deviceconnect.android.deviceplugin.host.HostDeviceService;
 import org.deviceconnect.android.event.EventError;
 import org.deviceconnect.android.event.EventManager;
@@ -16,10 +19,17 @@ import org.deviceconnect.android.message.MessageUtils;
 import org.deviceconnect.android.profile.PhoneProfile;
 import org.deviceconnect.message.DConnectMessage;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.AudioManager;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.ResultReceiver;
 
 /**
  * Phoneプロファイル.
@@ -42,28 +52,91 @@ public class HostPhoneProfile extends PhoneProfile {
             createNotFoundService(response);
         } else {
             if (phoneNumber != null) {
-                if (!checkPhoneNumber(phoneNumber)) {
-                    MessageUtils.setInvalidRequestParameterError(response,
-                            "phoneNumber is invalid.");
-                    return true;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    final String[] requiredPermissions = new String[] { Manifest.permission.CALL_PHONE };
+                    final List<String> missingPermissions = new ArrayList<>();
+                    for (String requiredPermission : requiredPermissions) {
+                        if (getContext().checkSelfPermission(requiredPermission) != PackageManager.PERMISSION_GRANTED) {
+                            missingPermissions.add(requiredPermission);
+                        }
+                    }
+                    if (missingPermissions.size() != 0) {
+                        PermissionRequestActivity.requestPermissions(getContext(),
+                                missingPermissions.toArray(new String[missingPermissions.size()]),
+                                new ResultReceiver(new Handler(Looper.getMainLooper())) {
+                                    @Override
+                                    protected void onReceiveResult(final int resultCode, final Bundle resultData) {
+                                        String[] permissions = resultData
+                                                .getStringArray(PermissionRequestActivity.EXTRA_PERMISSIONS);
+                                        int[] grantResults = resultData
+                                                .getIntArray(PermissionRequestActivity.EXTRA_GRANT_RESULTS);
+
+                                        if (permissions == null || permissions.length != missingPermissions.size()
+                                                || grantResults == null
+                                                || grantResults.length != missingPermissions.size()) {
+                                            MessageUtils.setIllegalServerStateError(response,
+                                                    "CALL_PHONE permission not granted.");
+                                            getContext().sendBroadcast(response);
+                                            return;
+                                        }
+
+                                        int count = missingPermissions.size();
+                                        for (String requiredPermission : missingPermissions) {
+                                            for (int i = 0; i < permissions.length; ++i) {
+                                                if (permissions[i].equals(requiredPermission)) {
+                                                    if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                                                        --count;
+                                                    } else {
+                                                        MessageUtils.setIllegalServerStateError(response,
+                                                                "CALL_PHONE permission not granted.");
+                                                        getContext().sendBroadcast(response);
+                                                        return;
+                                                    }
+                                                }
+                                            }
+                                        }
+
+                                        if (count == 0) {
+                                            onPostCallInternal(request, response, phoneNumber);
+                                            getContext().sendBroadcast(response);
+                                        } else {
+                                            MessageUtils.setIllegalServerStateError(response,
+                                                    "CALL_PHONE permission not granted.");
+                                            getContext().sendBroadcast(response);
+                                            return;
+                                        }
+                                    }
+                                });
+                        return false;
+                    }
                 }
-                
-                Uri uri = Uri.parse("tel:" + phoneNumber);
-                if (uri != null) {
-                    Intent intent = new Intent(Intent.ACTION_CALL, uri);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    getContext().startActivity(intent);
-                    setResult(response, DConnectMessage.RESULT_OK);
-                } else {
-                    MessageUtils.setInvalidRequestParameterError(response,
-                            "phoneNumber is invalid.");
-                }
+                onPostCallInternal(request, response, phoneNumber);
             } else {
-                MessageUtils.setInvalidRequestParameterError(response,
-                        "phoneNumber is invalid.");
+                MessageUtils.setInvalidRequestParameterError(response, "phoneNumber is invalid.");
             }
         }
         return true;
+    }
+
+    private void onPostCallInternal(final Intent request, final Intent response, final String phoneNumber) {
+        try {
+            if (!checkPhoneNumber(phoneNumber)) {
+                MessageUtils.setInvalidRequestParameterError(response, "phoneNumber is invalid.");
+                return;
+            }
+
+            Uri uri = Uri.parse("tel:" + phoneNumber);
+            if (uri != null) {
+                Intent intent = new Intent(Intent.ACTION_CALL, uri);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                getContext().startActivity(intent);
+                setResult(response, DConnectMessage.RESULT_OK);
+            } else {
+                MessageUtils.setInvalidRequestParameterError(response, "phoneNumber is invalid.");
+            }
+        } catch (Throwable throwable) {
+            MessageUtils.setUnknownError(response, "Failed to make a phone call.");
+        }
     }
 
     @Override
@@ -75,8 +148,7 @@ public class HostPhoneProfile extends PhoneProfile {
             createNotFoundService(response);
         } else {
             // AudioManager
-            AudioManager mAudioManager = (AudioManager) getContext()
-                    .getSystemService(Context.AUDIO_SERVICE);
+            AudioManager mAudioManager = (AudioManager) getContext().getSystemService(Context.AUDIO_SERVICE);
 
             if (mode.equals(PhoneMode.SILENT)) {
                 mAudioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
@@ -88,8 +160,7 @@ public class HostPhoneProfile extends PhoneProfile {
                 mAudioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
                 setResult(response, DConnectMessage.RESULT_OK);
             } else if (mode.equals(PhoneMode.UNKNOWN)) {
-                MessageUtils.setInvalidRequestParameterError(response,
-                        "mode is invalid.");
+                MessageUtils.setInvalidRequestParameterError(response, "mode is invalid.");
             }
         }
         return true;
@@ -207,6 +278,7 @@ public class HostPhoneProfile extends PhoneProfile {
 
     /**
      * 電話番号のフォーマットチェックを行う.
+     * 
      * @param phoneNumber 電話番号
      * @return 電話番号の場合はtrue、それ以外はfalse
      */

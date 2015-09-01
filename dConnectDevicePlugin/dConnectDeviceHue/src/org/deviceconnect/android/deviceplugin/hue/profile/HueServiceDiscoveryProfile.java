3e52679668d358cf106b2d1abc285cd1c53a71a5
小林伸郎 <nobu@gclue.jp>
2015-09-01 19:45:26 +0900

    Modified to conform to the new Light profile

--- dConnectDevicePlugin/dConnectDeviceHue/src/org/deviceconnect/android/deviceplugin/hue/profile/HueServiceDiscoveryProfile.java
+++ dConnectDevicePlugin/dConnectDeviceHue/src/org/deviceconnect/android/deviceplugin/hue/profile/HueServiceDiscoveryProfile.java
@@ -7,19 +7,18 @@ http://opensource.org/licenses/mit-license.php
 
 package org.deviceconnect.android.deviceplugin.hue.profile;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.deviceconnect.android.profile.DConnectProfileProvider;
+import org.deviceconnect.android.profile.ServiceDiscoveryProfile;
+import org.deviceconnect.message.DConnectMessage;
+
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.philips.lighting.hue.sdk.PHAccessPoint;
 import com.philips.lighting.hue.sdk.PHHueSDK;
-
-import org.deviceconnect.android.profile.DConnectProfileProvider;
-import org.deviceconnect.android.profile.ServiceDiscoveryProfile;
-import org.deviceconnect.message.DConnectMessage;
-import org.deviceconnect.profile.ServiceDiscoveryProfileConstants;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * スマートデバイス検索機能を提供するAPI.
@@ -46,10 +45,10 @@ public class HueServiceDiscoveryProfile extends ServiceDiscoveryProfile {
                 continue;
             }
             Bundle service = new Bundle();
-            service.putString(ServiceDiscoveryProfileConstants.PARAM_ID, accessPoint.getIpAddress());
-            service.putString(ServiceDiscoveryProfileConstants.PARAM_NAME, "hue " + accessPoint.getMacAddress());
-            service.putString(ServiceDiscoveryProfileConstants.PARAM_TYPE, "wifi");
-            service.putBoolean(ServiceDiscoveryProfileConstants.PARAM_ONLINE, true);
+            setId(service, accessPoint.getIpAddress());
+            setName(service, "hue " + accessPoint.getMacAddress());
+            setType(service, NetworkType.WIFI);
+            setOnline(service, true);
             setScopes(service, getProfileProvider());
             services.add(service);
         }

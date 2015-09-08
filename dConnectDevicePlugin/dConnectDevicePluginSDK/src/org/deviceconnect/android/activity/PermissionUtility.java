4fcb5c036e0f7020ed143dc0df20aa9d7267c92e
Shigekazu Fukui <fukui@gclue.jp>
2015-09-08 15:25:25 +0900

    In Device Plugin SDK, fix a utility class to make a permission request.
    Now, onSuccess() is immediately called in SDK 22 and below.

--- dConnectDevicePlugin/dConnectDevicePluginSDK/src/org/deviceconnect/android/activity/PermissionUtility.java
+++ dConnectDevicePlugin/dConnectDevicePluginSDK/src/org/deviceconnect/android/activity/PermissionUtility.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.ResultReceiver;
@@ -21,40 +22,44 @@ public class PermissionUtility {
 
     public static void requestPermissions(@NonNull final Context context, @NonNull final Handler handler,
             @NonNull final String[] permissions, @NonNull final PermissionRequestCallback callback) {
-        Set<String> mPermissionSet = new HashSet<>(Arrays.asList(permissions));
-
-        if (mPermissionSet.size() == 0) {
-            callback.onSuccess();
-            return;
-        }
-        Set<String> tmp = new HashSet<>(mPermissionSet);
-        for (final String permission : tmp) {
-            if (context.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED) {
-                mPermissionSet.remove(permission);
-            }
-        }
-        if (mPermissionSet.size() == 0) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
             callback.onSuccess();
         } else {
-            String[] missingPermissions = mPermissionSet.toArray(new String[mPermissionSet.size()]);
-            PermissionRequestActivity.requestPermissions(context, missingPermissions, new ResultReceiver(handler) {
-                @Override
-                protected void onReceiveResult(final int resultCode, final Bundle resultData) {
-                    String[] retPermissions = resultData.getStringArray(PermissionRequestActivity.EXTRA_PERMISSIONS);
-                    int[] retGrantResults = resultData.getIntArray(PermissionRequestActivity.EXTRA_GRANT_RESULTS);
-                    if (retPermissions == null || retGrantResults == null) {
-                        callback.onFail(null);
-                        return;
-                    }
-                    for (int i = 0; i < retPermissions.length; ++i) {
-                        if (retGrantResults[i] == PackageManager.PERMISSION_DENIED) {
-                            callback.onFail(retPermissions[i]);
+            Set<String> mPermissionSet = new HashSet<>(Arrays.asList(permissions));
+
+            if (mPermissionSet.size() == 0) {
+                callback.onSuccess();
+                return;
+            }
+            Set<String> tmp = new HashSet<>(mPermissionSet);
+            for (final String permission : tmp) {
+                if (context.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED) {
+                    mPermissionSet.remove(permission);
+                }
+            }
+            if (mPermissionSet.size() == 0) {
+                callback.onSuccess();
+            } else {
+                String[] missingPermissions = mPermissionSet.toArray(new String[mPermissionSet.size()]);
+                PermissionRequestActivity.requestPermissions(context, missingPermissions, new ResultReceiver(handler) {
+                    @Override
+                    protected void onReceiveResult(final int resultCode, final Bundle resultData) {
+                        String[] retPermissions = resultData.getStringArray(PermissionRequestActivity.EXTRA_PERMISSIONS);
+                        int[] retGrantResults = resultData.getIntArray(PermissionRequestActivity.EXTRA_GRANT_RESULTS);
+                        if (retPermissions == null || retGrantResults == null) {
+                            callback.onFail(null);
                             return;
                         }
+                        for (int i = 0; i < retPermissions.length; ++i) {
+                            if (retGrantResults[i] == PackageManager.PERMISSION_DENIED) {
+                                callback.onFail(retPermissions[i]);
+                                return;
+                            }
+                        }
+                        callback.onSuccess();
                     }
-                    callback.onSuccess();
-                }
-            });
+                });
+            }
         }
     }
 

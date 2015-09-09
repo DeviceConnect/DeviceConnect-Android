583302011062746a8be09d967d5ddb3f4624db8b
Shigekazu Fukui <fukui@gclue.jp>
2015-09-09 11:22:10 +0900

    In Device Plugin SDK, fix utility classes to make a permission request.
    This fix prevents a permission request UI from being displayed multiple times.

--- dConnectDevicePlugin/dConnectDevicePluginSDK/src/org/deviceconnect/android/activity/IntentHandlerActivity.java
+++ dConnectDevicePlugin/dConnectDevicePluginSDK/src/org/deviceconnect/android/activity/IntentHandlerActivity.java
@@ -19,58 +19,63 @@ public class IntentHandlerActivity extends Activity {
     private static final String EXTRA_CALLBACK = "EXTRA_CALLBACK";
     private static final int REQUEST_CODE = 123456789;
 
-    protected ResultReceiver mCallback;
-    protected Intent mIntent;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_transparent);
+        // setContentView(R.layout.activity_transparent);
 
-        Intent callingIntent = getIntent();
-        if (callingIntent == null || callingIntent.getExtras() == null ||
-                !callingIntent.hasExtra(EXTRA_INTENT) ||
-                !callingIntent.hasExtra(EXTRA_CALLBACK)) {
-            finish();
-            return;
-        }
+        if (savedInstanceState == null) {
+            Intent callingIntent = getIntent();
+            if (callingIntent == null || callingIntent.getExtras() == null || !callingIntent.hasExtra(EXTRA_INTENT)
+                    || !callingIntent.hasExtra(EXTRA_CALLBACK)) {
+                finish();
+                return;
+            }
 
-        Bundle extras = callingIntent.getExtras();
-        mIntent = extras.getParcelable(EXTRA_INTENT);
-        if (mIntent == null) {
-            finish();
-            return;
-        }
-        mCallback = extras.getParcelable(EXTRA_CALLBACK);
-        if (mCallback == null) {
-            finish();
-            return;
-        }
+            Bundle extras = callingIntent.getExtras();
+            Intent intent = extras.getParcelable(EXTRA_INTENT);
+            if (intent == null) {
+                finish();
+                return;
+            }
 
-        mIntent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-        startActivityForResult(mIntent, REQUEST_CODE);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+            startActivityForResult(intent, REQUEST_CODE);
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_CODE) {
+            Intent callingIntent = getIntent();
+            if (callingIntent == null || callingIntent.getExtras() == null || !callingIntent.hasExtra(EXTRA_CALLBACK)) {
+                finish();
+                return;
+            }
+
+            Bundle extras = callingIntent.getExtras();
+            ResultReceiver callback = extras.getParcelable(EXTRA_CALLBACK);
+            if (callback == null) {
+                finish();
+                return;
+            }
+
             Bundle dataBundle = new Bundle();
             dataBundle.putParcelable(EXTRA_INTENT_DATA, data);
-            mCallback.send(resultCode, dataBundle);
+            callback.send(resultCode, dataBundle);
             finish();
         }
     }
 
-    public static void startActivityForResult(@NonNull final Context context,
-                                              @NonNull Intent intent,
-                                              @NonNull ResultReceiver resultReceiver) {
+    public static void startActivityForResult(@NonNull final Context context, @NonNull Intent intent,
+            @NonNull ResultReceiver resultReceiver) {
         final Intent callIntent = new Intent(context, IntentHandlerActivity.class);
         callIntent.putExtra(EXTRA_INTENT, intent);
         callIntent.putExtra(EXTRA_CALLBACK, resultReceiver);
-        // NOTE: FLAG_ACTIVITY_SINGLE_TASK causes Activity#onActivityResult() being called
-        // prematurely. FLAG_ACTIVITY_SINGLE_TOP, on the other hand, does not cause that.
-        callIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                | Intent.FLAG_ACTIVITY_SINGLE_TOP
+        // NOTE: FLAG_ACTIVITY_SINGLE_TASK causes Activity#onActivityResult()
+        // being called prematurely. FLAG_ACTIVITY_SINGLE_TOP, on the other
+        // hand, does not cause that.
+        callIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP
                 | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
         context.startActivity(callIntent);
     }
--- dConnectDevicePlugin/dConnectDevicePluginSDK/src/org/deviceconnect/android/activity/PermissionRequestActivity.java
+++ dConnectDevicePlugin/dConnectDevicePluginSDK/src/org/deviceconnect/android/activity/PermissionRequestActivity.java
@@ -8,7 +8,8 @@ import android.os.ResultReceiver;
 import android.support.annotation.NonNull;
 
 /**
- * An Activity with interfaces to make a request for permissions and return the result.
+ * An Activity with interfaces to make a request for permissions and return the
+ * result.
  *
  * @author NTT DOCOMO, INC.
  */
@@ -19,55 +20,61 @@ public class PermissionRequestActivity extends Activity {
     private static final String EXTRA_CALLBACK = "EXTRA_CALLBACK";
     private static final int REQUEST_CODE = 123456789;
 
-    private ResultReceiver mCallback;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (savedInstanceState == null) {
+            Intent callingIntent = getIntent();
+            if (callingIntent == null || callingIntent.getExtras() == null || !callingIntent.hasExtra(EXTRA_PERMISSIONS)
+                    || !callingIntent.hasExtra(EXTRA_CALLBACK)) {
+                finish();
+                return;
+            }
+
+            Bundle extras = callingIntent.getExtras();
+            String[] permissions = extras.getStringArray(EXTRA_PERMISSIONS);
+            if (permissions == null) {
+                finish();
+                return;
+            }
+
+            requestPermissions(permissions, REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+            @NonNull int[] grantResults) {
         Intent callingIntent = getIntent();
-        if (callingIntent == null || callingIntent.getExtras() == null
-                || !callingIntent.hasExtra(EXTRA_PERMISSIONS)
-                || !callingIntent.hasExtra(EXTRA_CALLBACK)) {
+        if (callingIntent == null || callingIntent.getExtras() == null || !callingIntent.hasExtra(EXTRA_CALLBACK)) {
             finish();
             return;
         }
 
         Bundle extras = callingIntent.getExtras();
-        String[] permissions = extras.getStringArray(EXTRA_PERMISSIONS);
-        if (permissions == null) {
-            finish();
-            return;
-        }
-        mCallback = extras.getParcelable(EXTRA_CALLBACK);
-        if (mCallback == null) {
+        ResultReceiver callback = extras.getParcelable(EXTRA_CALLBACK);
+        if (callback == null) {
             finish();
             return;
         }
 
-        requestPermissions(permissions, REQUEST_CODE);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
         Bundle resultData = new Bundle();
         resultData.putStringArray(EXTRA_PERMISSIONS, permissions);
         resultData.putIntArray(EXTRA_GRANT_RESULTS, grantResults);
-        mCallback.send(Activity.RESULT_OK, resultData);
+        callback.send(Activity.RESULT_OK, resultData);
         finish();
     }
 
-    public static void requestPermissions(@NonNull Context context,
-                                          @NonNull String[] permissions,
-                                          @NonNull ResultReceiver resultReceiver) {
+    public static void requestPermissions(@NonNull Context context, @NonNull String[] permissions,
+            @NonNull ResultReceiver resultReceiver) {
         Intent callIntent = new Intent(context, PermissionRequestActivity.class);
         callIntent.putExtra(EXTRA_PERMISSIONS, permissions);
         callIntent.putExtra(EXTRA_CALLBACK, resultReceiver);
-        // NOTE: FLAG_ACTIVITY_SINGLE_TASK causes Activity#onActivityResult() being called
-        // prematurely. FLAG_ACTIVITY_SINGLE_TOP, on the other hand, does not cause that.
-        callIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                | Intent.FLAG_ACTIVITY_SINGLE_TOP
+        // NOTE: FLAG_ACTIVITY_SINGLE_TASK causes Activity#onActivityResult()
+        // being called prematurely. FLAG_ACTIVITY_SINGLE_TOP, on the other
+        // hand, does not cause that.
+        callIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP
                 | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
         context.startActivity(callIntent);
     }

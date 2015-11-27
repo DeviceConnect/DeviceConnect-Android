55a4f34e405e706a1f1eaf7d7f028d1f35e5782f
TakayukiHoshi1984 <hoshi@gclue.jp>
2015-11-27 18:49:07 +0900

    Fix to a horizontal screen.

--- dConnectDevicePlugin/dConnectDeviceTheta/app/src/main/java/org/deviceconnect/android/deviceplugin/theta/activity/ThetaFeatureActivity.java
+++ dConnectDevicePlugin/dConnectDeviceTheta/app/src/main/java/org/deviceconnect/android/deviceplugin/theta/activity/ThetaFeatureActivity.java
@@ -70,11 +70,7 @@ public class ThetaFeatureActivity extends FragmentActivity {
     @Override
     public void onResume() {
         super.onResume();
-        if (mMode == MODE_SHOOTING) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
-        } else if (mMode == MODE_VR) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        }
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 
     @Override

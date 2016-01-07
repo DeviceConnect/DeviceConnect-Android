e83af87949a109a22b1a04f3786c569c693da454
Masaru Takano <mtaka@gclue.jp>
2016-01-07 19:18:12 +0900

    Implement Service Information API of UVC Device Plug-in.

--- dConnectDevicePlugin/dConnectDeviceUVC/app/src/main/java/org/deviceconnect/android/deviceplugin/uvc/profile/UVCServiceInformationProfile.java
+++ dConnectDevicePlugin/dConnectDeviceUVC/app/src/main/java/org/deviceconnect/android/deviceplugin/uvc/profile/UVCServiceInformationProfile.java
@@ -7,6 +7,11 @@
 package org.deviceconnect.android.deviceplugin.uvc.profile;
 
 
+import android.content.Intent;
+import android.os.Bundle;
+
+import org.deviceconnect.android.deviceplugin.uvc.UVCDeviceManager;
+import org.deviceconnect.android.deviceplugin.uvc.UVCDeviceService;
 import org.deviceconnect.android.profile.DConnectProfileProvider;
 import org.deviceconnect.android.profile.ServiceInformationProfile;
 
@@ -17,7 +22,21 @@ import org.deviceconnect.android.profile.ServiceInformationProfile;
  */
 public class UVCServiceInformationProfile extends ServiceInformationProfile{
 
+    private static final String PARAM_USB = "usb";
+
+    private final UVCDeviceManager mDeviceMgr;
+
     public UVCServiceInformationProfile(final DConnectProfileProvider provider) {
         super(provider);
+        mDeviceMgr = ((UVCDeviceService) provider).getDeviceManager();
+    }
+
+    @Override
+    protected boolean onGetInformation(final Intent request, final Intent response,
+                                       final String serviceId) {
+        super.onGetInformation(request, response, serviceId);
+        Bundle connect = response.getBundleExtra(PARAM_CONNECT);
+        connect.putBoolean(PARAM_USB, mDeviceMgr.getDevice(serviceId) != null);
+        return true;
     }
 }

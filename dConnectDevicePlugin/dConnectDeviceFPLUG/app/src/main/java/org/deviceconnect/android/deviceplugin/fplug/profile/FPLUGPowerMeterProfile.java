b6e83953e7a84fc65c54f18311b0cd5b741755fb
Masaru Takano <mtaka@gclue.jp>
2016-07-06 12:52:36 +0900

    新しいSDKの作り方をプラグインに反映。
    - FPLUGプラグイン

--- dConnectDevicePlugin/dConnectDeviceFPLUG/app/src/main/java/org/deviceconnect/android/deviceplugin/fplug/profile/FPLUGPowerMeterProfile.java
+++ dConnectDevicePlugin/dConnectDeviceFPLUG/app/src/main/java/org/deviceconnect/android/deviceplugin/fplug/profile/FPLUGPowerMeterProfile.java
@@ -19,6 +19,8 @@ import org.deviceconnect.android.deviceplugin.fplug.fplug.WattHour;
 import org.deviceconnect.android.message.MessageUtils;
 import org.deviceconnect.android.profile.PowerMeterProfile;
 import org.deviceconnect.android.profile.PowerMeterProfileConstants;
+import org.deviceconnect.android.profile.api.DConnectApi;
+import org.deviceconnect.android.profile.api.GetApi;
 import org.deviceconnect.message.DConnectMessage;
 
 import java.text.SimpleDateFormat;
@@ -36,83 +38,104 @@ public class FPLUGPowerMeterProfile extends PowerMeterProfile {
 
     private final static String RFC_3339 = "yyyy-MM-dd'T'HH:mm:ssZ";
 
-    @Override
-    protected boolean onGetIntegratedPowerValue(Intent request, final Intent response) {
-        final String serviceId = request.getStringExtra(PARAM_SERVICE_ID);
-        String date = request.getStringExtra(PARAM_DATE);
-        Calendar calendar;
-        if (date != null) {
-            calendar = createCalendar(date);
-            if (calendar == null) {
-                MessageUtils.setInvalidRequestParameterError(response, "date parse error");
-                sendResultError(response);
-                return true;
-            }
-        } else {
-            calendar = Calendar.getInstance();
+    private final DConnectApi mGetIntegratedPowerValueApi = new GetApi() {
+
+        @Override
+        public String getAttribute() {
+            return ATTR_INTEGRATEDPOWERVALUE;
         }
-        FPLUGApplication app = ((FPLUGApplication) getContext().getApplicationContext());
-        FPLUGController controller = app.getConnectedController(serviceId);
-        if (controller == null) {
-            MessageUtils.setUnknownError(response, "F-PLUG not connected");
-            sendResultError(response);
-            return false;
-        }
-        controller.requestPastWattHour(calendar, new FPLUGRequestCallback() {
-            @Override
-            public void onSuccess(FPLUGResponse fResponse) {
-                List<WattHour> wattHourList = fResponse.getWattHourList();
-                int[] wattList = new int[wattHourList.size()];
-                for (int i = 0; i < wattHourList.size(); i++) {
-                    wattList[i] = wattHourList.get(i).getWatt();
+
+        @Override
+        public boolean onRequest(final Intent request, final Intent response) {
+            final String serviceId = getServiceID(request);
+            String date = request.getStringExtra(PARAM_DATE);
+            Calendar calendar;
+            if (date != null) {
+                calendar = createCalendar(date);
+                if (calendar == null) {
+                    MessageUtils.setInvalidRequestParameterError(response, "date parse error");
+                    sendResultError(response);
+                    return true;
                 }
-                response.putExtra(PowerMeterProfileConstants.PARAM_INTEGRATEDPOWERVALUE, wattList);
-                sendResultOK(response);
+            } else {
+                calendar = Calendar.getInstance();
             }
-
-            @Override
-            public void onError(String message) {
-                MessageUtils.setUnknownError(response, message);
+            FPLUGApplication app = ((FPLUGApplication) getContext().getApplicationContext());
+            FPLUGController controller = app.getConnectedController(serviceId);
+            if (controller == null) {
+                MessageUtils.setUnknownError(response, "F-PLUG not connected");
                 sendResultError(response);
+                return false;
             }
+            controller.requestPastWattHour(calendar, new FPLUGRequestCallback() {
+                @Override
+                public void onSuccess(FPLUGResponse fResponse) {
+                    List<WattHour> wattHourList = fResponse.getWattHourList();
+                    int[] wattList = new int[wattHourList.size()];
+                    for (int i = 0; i < wattHourList.size(); i++) {
+                        wattList[i] = wattHourList.get(i).getWatt();
+                    }
+                    response.putExtra(PowerMeterProfileConstants.PARAM_INTEGRATEDPOWERVALUE, wattList);
+                    sendResultOK(response);
+                }
 
-            @Override
-            public void onTimeout() {
-                sendResultTimeout(response);
-            }
-        });
-        return false;
-    }
+                @Override
+                public void onError(String message) {
+                    MessageUtils.setUnknownError(response, message);
+                    sendResultError(response);
+                }
 
-    @Override
-    protected boolean onGetInstantaneousPowerValue(Intent request, final Intent response) {
-        final String serviceId = request.getStringExtra(PARAM_SERVICE_ID);
-        FPLUGApplication app = ((FPLUGApplication) getContext().getApplicationContext());
-        FPLUGController controller = app.getConnectedController(serviceId);
-        if (controller == null) {
-            MessageUtils.setUnknownError(response, "F-PLUG not connected");
-            sendResultError(response);
+                @Override
+                public void onTimeout() {
+                    sendResultTimeout(response);
+                }
+            });
             return false;
         }
-        controller.requestRealtimeWatt(new FPLUGRequestCallback() {
-            @Override
-            public void onSuccess(FPLUGResponse fResponse) {
-                response.putExtra(PowerMeterProfileConstants.PARAM_INSTANTANEOUSPOWERVALUE, fResponse.getRealtimeWatt());
-                sendResultOK(response);
-            }
+    };
 
-            @Override
-            public void onError(String message) {
-                MessageUtils.setUnknownError(response, message);
+    private final DConnectApi mGetInstantaneousPowerValueApi = new GetApi() {
+
+        @Override
+        public String getAttribute() {
+            return ATTR_INSTANTANEOUSPOWERVALUE;
+        }
+
+        @Override
+        public boolean onRequest(final Intent request, final Intent response) {
+            final String serviceId = request.getStringExtra(PARAM_SERVICE_ID);
+            FPLUGApplication app = ((FPLUGApplication) getContext().getApplicationContext());
+            FPLUGController controller = app.getConnectedController(serviceId);
+            if (controller == null) {
+                MessageUtils.setUnknownError(response, "F-PLUG not connected");
                 sendResultError(response);
+                return false;
             }
+            controller.requestRealtimeWatt(new FPLUGRequestCallback() {
+                @Override
+                public void onSuccess(FPLUGResponse fResponse) {
+                    response.putExtra(PowerMeterProfileConstants.PARAM_INSTANTANEOUSPOWERVALUE, fResponse.getRealtimeWatt());
+                    sendResultOK(response);
+                }
 
-            @Override
-            public void onTimeout() {
-                sendResultTimeout(response);
-            }
-        });
-        return false;
+                @Override
+                public void onError(String message) {
+                    MessageUtils.setUnknownError(response, message);
+                    sendResultError(response);
+                }
+
+                @Override
+                public void onTimeout() {
+                    sendResultTimeout(response);
+                }
+            });
+            return false;
+        }
+    };
+
+    public FPLUGPowerMeterProfile() {
+        addApi(mGetIntegratedPowerValueApi);
+        addApi(mGetInstantaneousPowerValueApi);
     }
 
     private Calendar createCalendar(String date) {

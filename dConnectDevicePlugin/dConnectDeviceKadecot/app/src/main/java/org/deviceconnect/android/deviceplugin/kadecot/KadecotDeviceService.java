b6d943776198ec1f89c3a9da7fd290cd920ecf45
TakeoHatakeyama <takeo@gclue.jp>
2016-08-02 19:16:33 +0900

    デバッグ用ログ修正。

--- dConnectDevicePlugin/dConnectDeviceKadecot/app/src/main/java/org/deviceconnect/android/deviceplugin/kadecot/KadecotDeviceService.java
+++ dConnectDevicePlugin/dConnectDeviceKadecot/app/src/main/java/org/deviceconnect/android/deviceplugin/kadecot/KadecotDeviceService.java
@@ -64,19 +64,25 @@ public class KadecotDeviceService extends DConnectMessageService {
     @Override
     protected void onManagerUninstalled() {
         // Managerアンインストール検知時の処理。
-        mLogger.info("Plug-in : onManagerUninstalled");
+        if (BuildConfig.DEBUG) {
+            mLogger.info("Plug-in : onManagerUninstalled");
+        }
     }
 
     @Override
     protected void onManagerTerminated() {
         // Manager正常終了通知受信時の処理。
-        mLogger.info("Plug-in : onManagerTerminated");
+        if (BuildConfig.DEBUG) {
+            mLogger.info("Plug-in : onManagerTerminated");
+        }
     }
 
     @Override
     protected void onDevicePluginReset() {
         // Device Plug-inへのReset要求受信時の処理。
-        mLogger.info("Plug-in : onDevicePluginReset");
+        if (BuildConfig.DEBUG) {
+            mLogger.info("Plug-in : onDevicePluginReset");
+        }
     }
 
     @Override

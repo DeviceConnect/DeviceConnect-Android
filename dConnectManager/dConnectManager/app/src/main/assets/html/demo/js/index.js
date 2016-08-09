8f51eb4c94ad7e82f3a288329261451585bfb382
小林伸郎 <nobu@gclue.jp>
2016-08-09 15:58:03 +0900

    デバイス確認画面のプロファイルのアイコンを追加

--- dConnectManager/dConnectManager/app/src/main/assets/html/demo/js/index.js
+++ dConnectManager/dConnectManager/app/src/main/assets/html/demo/js/index.js
@@ -1,5 +1,23 @@
 
 var main = (function(parent, global) {
+
+    var mIconList = {
+        'battery' : 'images/icon01_battery.png',
+        'mediastreamrecording' : 'images/icon02_mediaREC.png',
+        'deviceorientation' : 'images/icon05_acceleration3.png',
+        'vibration' : 'images/icon07_vibration.png',
+        'settings' : 'images/icon08_setting.png',
+        'serviceinformation' : 'images/icon08_setting.png',
+        'canvas' : 'images/icon09_canvas.png',
+        'mediaplayer' : 'images/icon10_mediaPlayer.png',
+        'notification' : 'images/icon11_notification.png',
+        'file' : 'images/icon12_file.png',
+        'filedescriptor' : 'images/icon12_file.png',
+        'proximity' : 'images/icon15_proximity3.png',
+        'phone' : 'images/icon17_phone.png',
+        'keyevent' : 'images/icon19_kye2.png',
+    };
+
     function init() {
         util.init(function(name, json) {
             setServiceInfo(name, util.getServiceId());
@@ -12,10 +30,11 @@ var main = (function(parent, global) {
         document.getElementById('serviceInfo').innerHTML = createServiceInfo(name, serviceId);
     }
 
-    function createProfileCell(url, content) {
+    function createProfileCell(url, content, icon) {
         var data = {
             'url' : url,
-            'content' : content
+            'content' : content,
+            'icon' : icon
         };
         return util.createTemplate('profileCell', data);
     }
@@ -44,7 +63,11 @@ var main = (function(parent, global) {
 
     function createProfile(profile) {
         var url = 'checker.html?serviceId=' + util.getServiceId() + '&profile=' + profile;
-        return createProfileCell(url, profile);
+        var icon = mIconList[profile.toLowerCase()];
+        if (!icon) {
+            icon = 'images/icon21_other.png';
+        }
+        return createProfileCell(url, profile, icon);
     }
 
     function createProfileList(profileList) {

37809ac4cf191a26f4fd5e667f45b0397c276019
Masaru Takano <mtaka@gclue.jp>
2016-08-19 22:06:42 +0900

    HVC-C2Wデバイスプラグインにサービス一覧画面を追加。

---
+++ dConnectDevicePlugin/dConnectDeviceHVCC2W/app/src/main/java/org/deviceconnect/android/deviceplugin/hvcc2w/setting/HVCC2WServiceListActivity.java
@@ -0,0 +1,32 @@
+/*
+ HVCC2WServiceListActivity
+ Copyright (c) 2016 NTT DOCOMO,INC.
+ Released under the MIT license
+ http://opensource.org/licenses/mit-license.php
+ */
+package org.deviceconnect.android.deviceplugin.hvcc2w.setting;
+
+
+import android.app.Activity;
+
+import org.deviceconnect.android.deviceplugin.hvcc2w.HVCC2WDeviceService;
+import org.deviceconnect.android.message.DConnectMessageService;
+import org.deviceconnect.android.ui.activity.DConnectServiceListActivity;
+
+/**
+ * HVC-C2Wサービス一覧画面.
+ *
+ * @author NTT DOCOMO, INC.
+ */
+public class HVCC2WServiceListActivity extends DConnectServiceListActivity {
+
+    @Override
+    protected Class<? extends DConnectMessageService> getMessageServiceClass() {
+        return HVCC2WDeviceService.class;
+    }
+
+    @Override
+    protected Class<? extends Activity> getSettingManualActivityClass() {
+        return SettingActivity.class;
+    }
+}

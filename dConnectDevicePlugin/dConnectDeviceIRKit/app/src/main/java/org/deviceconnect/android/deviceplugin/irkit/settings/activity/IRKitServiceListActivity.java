58a8e1c2e52e9f0cf00f81726a259c4405e169ed
Masaru Takano <mtaka@gclue.jp>
2016-08-26 11:18:41 +0900

    IRKitのロスト検知のタイミングを調整。
    基本的には 512秒 (= 8分32秒) ごとに確認する。
    ただし、接続の確認をしやすくするために、最初の 512 秒間は、8秒という短い間隔で確認する。

--- dConnectDevicePlugin/dConnectDeviceIRKit/app/src/main/java/org/deviceconnect/android/deviceplugin/irkit/settings/activity/IRKitServiceListActivity.java
+++ dConnectDevicePlugin/dConnectDeviceIRKit/app/src/main/java/org/deviceconnect/android/deviceplugin/irkit/settings/activity/IRKitServiceListActivity.java
@@ -9,7 +9,6 @@ package org.deviceconnect.android.deviceplugin.irkit.settings.activity;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v4.content.LocalBroadcastManager;
 
 import org.deviceconnect.android.deviceplugin.irkit.IRKitDeviceService;
 import org.deviceconnect.android.deviceplugin.irkit.service.IRKitService;
@@ -17,34 +16,11 @@ import org.deviceconnect.android.message.DConnectMessageService;
 import org.deviceconnect.android.service.DConnectService;
 import org.deviceconnect.android.ui.activity.DConnectServiceListActivity;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
-
 /**
  * IRKitサービス一覧画面.
  * @author NTT DOCOMO, INC.
  */
 public class IRKitServiceListActivity extends DConnectServiceListActivity {
-    /**
-     * 初回実行待ち時間.
-     */
-    private static final int CHK_FIRST_WAIT_PERIOD = 5 * 1000;
-
-    /**
-     * 周期.
-     */
-    private static final int CHK_WAIT_PERIOD = 5 * 1000;
-    /**
-     * ScheduledExecutorServiceのインスタンス.
-     */
-    private ScheduledExecutorService mExecutor = Executors.newSingleThreadScheduledExecutor();
-
-    /**
-     * サービス検索タイマー.
-     */
-    private ScheduledFuture<?> mAutoConnectTimerFuture;
 
     @Override
     protected Class<? extends DConnectMessageService> getMessageServiceClass() {
@@ -77,43 +53,5 @@ public class IRKitServiceListActivity extends DConnectServiceListActivity {
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.putExtra(IRKitVirtualDeviceListActivity.EXTRA_SERVICE_ID, service.getId());
         startActivity(intent);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        start();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        stop();
-    }
-
-    /**
-     * タイマーのスタート.
-     */
-    private synchronized void start() {
-        if (mAutoConnectTimerFuture != null) {
-            return;
-        }
-        mAutoConnectTimerFuture = mExecutor.scheduleAtFixedRate(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(IRKitDeviceService.ACTION_RESTART_DETECTION_IRKIT);
-                LocalBroadcastManager.getInstance(IRKitServiceListActivity.this).sendBroadcast(i);
-            }
-        }, CHK_FIRST_WAIT_PERIOD, CHK_WAIT_PERIOD, TimeUnit.MILLISECONDS);
-    }
-
-    /**
-     * タイマーのストップ.
-     */
-    private synchronized void stop() {
-        if (mAutoConnectTimerFuture != null) {
-            mAutoConnectTimerFuture.cancel(true);
-            mAutoConnectTimerFuture = null;
-        }
     }
 }

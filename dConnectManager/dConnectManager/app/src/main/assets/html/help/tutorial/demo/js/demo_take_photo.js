8b76f4fd9e902d855a0f7a0262b2d23c8c3a5975
小林伸郎 <nobu@gclue.jp>
2017-01-25 17:46:30 +0900

    ヘルプ画面に省電力についてを追加

--- dConnectManager/dConnectManager/app/src/main/assets/html/help/tutorial/demo/js/demo_take_photo.js
+++ dConnectManager/dConnectManager/app/src/main/assets/html/help/tutorial/demo/js/demo_take_photo.js
@@ -32,7 +32,7 @@ var demoTakePhoto = (function(parent, global) {
             mMediaRecorders = json.recorders;
             createMediaRecorders();
         }, function(errorCode, errorMessage) {
-            util.showAlert("カメラ情報の取得に失敗しました。。", errorCode, errorMessage);
+            util.showAlert('カメラ情報の取得に失敗しました。。', errorCode, errorMessage);
         });
     }
 
@@ -51,7 +51,7 @@ var demoTakePhoto = (function(parent, global) {
             mPreviewSizes = json.previewSizes;
             createOptions();
         }, function(errorCode, errorMessage) {
-            util.showAlert("カメラ情報の取得に失敗しました。。", errorCode, errorMessage);
+            util.showAlert('カメラ情報の取得に失敗しました。。', errorCode, errorMessage);
         });
     }
 
@@ -74,7 +74,7 @@ var demoTakePhoto = (function(parent, global) {
         for (var i = 0; i < mImageSizes.length; i++) {
             var option = document.createElement('option');
             option.setAttribute('value', i);
-            option.innerHTML = mImageSizes[i].width + "x" + mImageSizes[i].height;
+            option.innerHTML = mImageSizes[i].width + 'x' + mImageSizes[i].height;
 
             if (mImageSizes[i].width < minWidth) {
                 option.selected = true;
@@ -89,7 +89,7 @@ var demoTakePhoto = (function(parent, global) {
         for (var i = 0; i < mPreviewSizes.length; i++) {
             var option = document.createElement('option');
             option.setAttribute('value', i);
-            option.innerHTML = mPreviewSizes[i].width + "x" + mPreviewSizes[i].height;
+            option.innerHTML = mPreviewSizes[i].width + 'x' + mPreviewSizes[i].height;
 
             if (mPreviewSizes[i].width < minWidth) {
                 option.selected = true;
@@ -115,7 +115,7 @@ var demoTakePhoto = (function(parent, global) {
         dConnect.put(uri, null, null, function(json) {
             refreshImg(json.uri, 'preview');
         }, function(errorCode, errorMessage) {
-            util.showAlert("プレビュー開始に失敗しました。", errorCode, errorMessage);
+            util.showAlert('プレビュー開始に失敗しました。', errorCode, errorMessage);
         });
     }
 
@@ -136,18 +136,37 @@ var demoTakePhoto = (function(parent, global) {
                 setTimeout(callback, 2000);
             }
         }, function(errorCode, errorMessage) {
-            util.showAlert("プレビュー停止に失敗しました。", errorCode, errorMessage);
+            util.showAlert('プレビュー停止に失敗しました。', errorCode, errorMessage);
         });
     }
 
-    function addPhoto(uri) {
-        var elem = document.createElement("img");
-        elem.setAttribute("src", uri);
-        elem.setAttribute("class", "photo")
-        elem.setAttribute("crossorigin", "anonymous")
-        elem.setAttribute("alt", "写真");
+    function showPhoto(uri) {
+        var elem = document.getElementById('photos');
+        elem.style.display = 'block';
+        elem.onclick = function() {
+             elem.style.display = 'none';
+        };
 
-        var tag = document.getElementById('photos');
+        var image = document.getElementById('photo');
+        image.setAttribute('src', uri);
+    }
+
+    function onClickPhoto(elem) {
+        var uri = elem.getAttribute("src")
+        showPhoto(uri);
+    }
+
+    function addPhoto(uri) {
+        var elem = document.createElement('img');
+        elem.setAttribute('src', uri);
+        elem.setAttribute('class', 'photo')
+        elem.setAttribute('crossorigin', 'anonymous')
+        elem.setAttribute('alt', '写真');
+        elem.onclick = function() {
+            showPhoto(uri);
+        };
+
+        var tag = document.getElementById('thumbnails');
         tag.appendChild(elem);
     }
 
@@ -166,7 +185,7 @@ var demoTakePhoto = (function(parent, global) {
         dConnect.post(uri, null, null, function(json) {
             addPhoto(json.uri);
         }, function(errorCode, errorMessage) {
-            util.showAlert("撮影に失敗しました。", errorCode, errorMessage);
+            util.showAlert('撮影に失敗しました。', errorCode, errorMessage);
         });
     }
     parent.onTakePhoto = onTakePhoto;
@@ -214,14 +233,14 @@ var demoTakePhoto = (function(parent, global) {
             builder.addParameter('imageHeight', imageHeight);
             builder.addParameter('previewWidth', previewWidth);
             builder.addParameter('previewHeight', previewHeight);
-            builder.addParameter('mimeType', "image/png");
+            builder.addParameter('mimeType', 'image/png');
             var uri = builder.build();
             dConnect.put(uri, null, null, function(json) {
                 if (mPreviewFlag) {
                     startPreview();
                 }
             }, function(errorCode, errorMessage) {
-                util.showAlert("設定に失敗しました。", errorCode, errorMessage);
+                util.showAlert('設定に失敗しました。', errorCode, errorMessage);
             });
         });
     }

a1b4ae7d4e8dbe5d630bc9b9cd45c525944a3e04
TakayukiHoshi1984 <hoshi@gclue.jp>
2017-06-01 17:24:59 +0900

    HVC-C2WがServiceDiscovery以外のタイミングでも見つかるように修正。

--- dConnectDevicePlugin/dConnectDeviceHVCC2W/app/src/main/java/org/deviceconnect/android/deviceplugin/hvcc2w/setting/HVCC2WServiceListActivity.java
+++ dConnectDevicePlugin/dConnectDeviceHVCC2W/app/src/main/java/org/deviceconnect/android/deviceplugin/hvcc2w/setting/HVCC2WServiceListActivity.java
@@ -8,10 +8,31 @@ package org.deviceconnect.android.deviceplugin.hvcc2w.setting;
 
 
 import android.app.Activity;
+import android.os.Handler;
+import android.util.Log;
 
+import org.deviceconnect.android.deviceplugin.hvcc2w.BuildConfig;
 import org.deviceconnect.android.deviceplugin.hvcc2w.HVCC2WDeviceService;
+import org.deviceconnect.android.deviceplugin.hvcc2w.manager.HVCManager;
+import org.deviceconnect.android.deviceplugin.hvcc2w.manager.data.HVCCameraInfo;
+import org.deviceconnect.android.deviceplugin.hvcc2w.service.HVCC2WService;
 import org.deviceconnect.android.message.DConnectMessageService;
+import org.deviceconnect.android.service.DConnectService;
+import org.deviceconnect.android.service.DConnectServiceProvider;
 import org.deviceconnect.android.ui.activity.DConnectServiceListActivity;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * HVC-C2Wサービス一覧画面.
@@ -19,8 +40,9 @@ import org.deviceconnect.android.ui.activity.DConnectServiceListActivity;
  * @author NTT DOCOMO, INC.
  */
 public class HVCC2WServiceListActivity extends DConnectServiceListActivity {
-
-    @Override
+    /** Timer 用Handler.*/
+    private final Handler mHandler = new Handler();
+   @Override
     protected Class<? extends DConnectMessageService> getMessageServiceClass() {
         return HVCC2WDeviceService.class;
     }
@@ -33,24 +55,92 @@ public class HVCC2WServiceListActivity extends DConnectServiceListActivity {
     @Override
     public void onResume() {
         super.onResume();
-        start();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                start();
+                mHandler.postDelayed(this, 5000);
+            }
+        }, 5000);
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        stop();
+        mHandler.removeCallbacksAndMessages(null);
     }
 
     /**
      * タイマーのスタート.
      */
     private synchronized void start() {
+        HVCManager.INSTANCE.getCameraList(new HVCManager.ResponseListener() {
+            @Override
+            public void onReceived(String json) {
+
+                final Map<String, HVCCameraInfo> devices = HVCManager.INSTANCE.getHVCDevices();
+                final CountDownLatch countDownLatch = new CountDownLatch(devices.size());
+                final DConnectServiceProvider provider = HVCC2WServiceListActivity.super.getMessageService().getServiceProvider();
+                final List<DConnectService> disappeared = provider.getServiceList();
+                for (String key : devices.keySet()) {
+                    final HVCCameraInfo camera = devices.get(key);
+                    HVCManager.INSTANCE.setCamera(camera.getID(), new HVCManager.ResponseListener() {
+                        @Override
+                        public void onReceived(String json) {
+                            if (json == null) {
+                                countDownLatch.countDown();
+                                return;
+                            }
+                            try {
+                                JSONObject result = new JSONObject(json);
+                                if (result.getInt("result") != 1) {
+                                    countDownLatch.countDown();
+                                    return;
+                                }
+                            } catch (JSONException e) {
+                                if (BuildConfig.DEBUG) {
+                                    e.printStackTrace();
+                                }
+                                countDownLatch.countDown();
+                                return;
+                            }
+
+                            // 検出されたデバイスについてのサービスを登録.
+                            DConnectService hvcService = provider.getService(camera.getID());
+                            if (hvcService == null) {
+                                hvcService = new HVCC2WService(camera);
+                                provider.addService(hvcService);
+                            } else {
+                                for (Iterator<DConnectService> it = disappeared.iterator(); ; it.hasNext()) {
+                                    DConnectService cache = it.next();
+                                    if (cache.getId().equals(hvcService.getId())) {
+                                        it.remove();
+                                        break;
+                                    }
+                                }
+                            }
+                            // 検出されたデバイスはオンライン状態とみなす.
+                            hvcService.setOnline(true);
+
+                            countDownLatch.countDown();
+                        }
+                    });
+                }
+                try {
+                    countDownLatch.await();
+
+                    // 消失したデバイスについてはオフライン状態とみなす.
+                    for (DConnectService service : disappeared) {
+                        service.setOnline(false);
+                    }
+                } catch (InterruptedException e) {
+                    if (BuildConfig.DEBUG) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 
-    /**
-     * タイマーのストップ.
-     */
-    private synchronized void stop() {
-    }
+
 }

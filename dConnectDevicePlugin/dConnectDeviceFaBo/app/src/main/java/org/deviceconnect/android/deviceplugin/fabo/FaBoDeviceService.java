99a754537426dc5d8869a8b5c9aebb4f20056c8c
小林伸郎 <nobu@gclue.jp>
2017-06-12 16:08:33 +0900

    デバッグ用フラグを削除

--- dConnectDevicePlugin/dConnectDeviceFaBo/app/src/main/java/org/deviceconnect/android/deviceplugin/fabo/FaBoDeviceService.java
+++ dConnectDevicePlugin/dConnectDeviceFaBo/app/src/main/java/org/deviceconnect/android/deviceplugin/fabo/FaBoDeviceService.java
@@ -99,8 +99,6 @@ public class FaBoDeviceService extends DConnectMessageService {
             }
         });
         mFaBoDeviceControl.initialize();
-
-        setOnline(true);
     }
 
     @Override

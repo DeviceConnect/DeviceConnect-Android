7921ed06d39394588d904c0348a9b44a6e9bcb98
Masaru Takano <mtaka@gclue.jp>
2017-11-17 11:41:21 +0900

    テスト用プラグインにサービス一覧画面を追加。
    下記のRESTコマンドで表示可能。
    
    (1) 追加ボタンあり
    curl -X PUT 'http://192.168.1.152:4035/gotapi/system/device/wakeup?pluginId=74f6f35e2c22eb4e51585f6a778f41.localhost.deviceconnect.org&show'
    
    (2) 追加ボタンなし
    curl -X PUm/device/wakeup?pluginId=74f6f35e2c22eb4e51585f6a778f41.localhost.deviceconnect.org&show&cannotAddService'

---
+++ dConnectDevicePlugin/dConnectDeviceTest/app/src/main/java/org/deviceconnect/android/deviceplugin/test/DummySettingManualActivity.java
@@ -0,0 +1,20 @@
+/*
+ DummySettingManualActivity.java
+ Copyright (c) 2017 NTT DOCOMO,INC.
+ Released under the MIT license
+ http://opensource.org/licenses/mit-license.php
+ */
+package org.deviceconnect.android.deviceplugin.test;
+
+import android.app.Activity;
+
+
+/**
+ * ダミーの設定手順画面.
+ *
+ * 空白の画面を表示.
+ *
+ * @author NTT DOCOMO, INC.
+ */
+public class DummySettingManualActivity extends Activity {
+}

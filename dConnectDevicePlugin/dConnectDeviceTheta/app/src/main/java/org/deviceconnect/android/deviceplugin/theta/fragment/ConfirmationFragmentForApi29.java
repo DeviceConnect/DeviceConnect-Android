5e716f726759b5abf92823f7305c4d2146bf24d0
Masaru Takano <mtaka@gclue.jp>
2019-09-19 21:32:39 +0900

    Android 10 上では Wi-Fi の ON/OFF はユーザー操作が必要。

--- dConnectDevicePlugin/dConnectDeviceTheta/app/src/main/java/org/deviceconnect/android/deviceplugin/theta/fragment/ConfirmationFragmentForApi29.java
+++ dConnectDevicePlugin/dConnectDeviceTheta/app/src/main/java/org/deviceconnect/android/deviceplugin/theta/fragment/ConfirmationFragmentForApi29.java
@@ -4,7 +4,9 @@ import android.annotation.TargetApi;
 import android.content.Intent;
 import android.net.wifi.ScanResult;
 import android.os.Build;
+import android.provider.Settings;
 
+import org.deviceconnect.android.deviceplugin.theta.R;
 import org.deviceconnect.android.deviceplugin.theta.ThetaDeviceService;
 
 @TargetApi(Build.VERSION_CODES.Q)
@@ -21,8 +23,17 @@ public class ConfirmationFragmentForApi29 extends BaseConfirmationFragment {
     @Override
     protected void onWifiDisabled() {
         mLogger.info("ConfirmationFragmentForApi29.onWifiDisabled");
-        // TODO API レベル 29 以降は Wi-Fi の ON/OFF をアプリで制御不能になったため、端末標準の設定画面へ誘導
+        // API レベル 29 以降は Wi-Fi の ON/OFF をアプリで制御不能になったため、端末標準の設定画面へ誘導
+        ThetaDialogFragment.showConfirmAlert(getActivity(),
+                getString(R.string.wifi_setting),
+                getString(R.string.wifi_setting_prompt),
+                getString(R.string.open_wifi_setting),
+                (dialog, which) -> openWifiSetting());
     }
 
+    private void openWifiSetting() {
+        Intent intent = new Intent(Settings.ACTION_WIFI_SETTINGS);
+        startActivity(intent);
+    }
 
 }

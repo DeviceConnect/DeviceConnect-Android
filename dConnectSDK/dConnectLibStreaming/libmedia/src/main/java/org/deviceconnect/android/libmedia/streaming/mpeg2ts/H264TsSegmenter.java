96dc7e5310bc204fba2c0350c4bf0b7968e14000
Masaru Takano <mtaka@gclue.jp>
2020-01-29 22:05:05 +0900

    H.264 NAL Unit 解析時にオブジェクトを生成しないように修正。

--- dConnectSDK/dConnectLibStreaming/libmedia/src/main/java/org/deviceconnect/android/libmedia/streaming/mpeg2ts/H264TsSegmenter.java
+++ dConnectSDK/dConnectLibStreaming/libmedia/src/main/java/org/deviceconnect/android/libmedia/streaming/mpeg2ts/H264TsSegmenter.java
@@ -7,8 +7,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-import static org.deviceconnect.android.libmedia.BuildConfig.DEBUG;
-
 
 public class H264TsSegmenter extends AbstractTsSegmenter {
 	
@@ -97,269 +95,40 @@ public class H264TsSegmenter extends AbstractTsSegmenter {
 		avcFrameCache.clear();
 	}
 
-	public void generatePackets(final ByteBuffer buffer, final long defaultPts) {
+	public void generatePackets(final ByteBuffer buffer) {
 		List<Integer> offsets = ByteUtil.kmp(buffer, START_CODE);
 		buffer.position(0);
 		int totalLength = buffer.remaining();
-		List<NALUnit> units = new ArrayList<>();
 		for (int i = 0; i < offsets.size(); i++) {
-			int unitOffset = offsets.get(i);
-			NALUnit unit = new NALUnit();
-			unit.offset = unitOffset;
+			final int unitOffset = offsets.get(i);
+			final int unitLength;
 			if (i == offsets.size() - 1) {
-				unit.length = totalLength - unitOffset;
+				unitLength = totalLength - unitOffset;
 			} else {
 				int nextOffset = offsets.get(i + 1);
-				unit.length = nextOffset - unitOffset;
+				unitLength = nextOffset - unitOffset;
 			}
-			unit.type = buffer.get(unitOffset + START_CODE.length) & 0x1F;
-			units.add(unit);
-		}
+			int type = buffer.get(unitOffset + START_CODE.length) & 0x1F;
 
-		for (NALUnit unit : units) {
-			boolean isFrame = unit.type == H264NT_SLICE || unit.type == H264NT_SLICE_IDR;
-			long pts = isFrame ? getPts() : -1;
-			long dts = pts;
-			buffer.position(unit.offset);
-			tsWriter.writeVideoBuffer(isFirstPes, buffer, unit.length, pts, dts, isFrame);
+			boolean isFrame = type == H264NT_SLICE || type == H264NT_SLICE_IDR;
+			long pts = getPts();
+			buffer.position(unitOffset);
+			tsWriter.writeVideoBuffer(isFirstPes, buffer, unitLength, pts, pts, isFrame);
 			isFirstPes = false;
 		}
-	}
-
-	private static class NALUnit {
-		private int offset;
-		private int length;
-		private int type;
-	}
-
-	public byte[] segment(ByteBuffer buffer, long pts) {
-
-		boolean isNalDelimiter4 = false;
-		byte nextNalType;
-		
-		int seekPos = framesBuf.size() >= NAL_DELIMITER.length ? framesBuf.size() - NAL_DELIMITER.length : 0;
-		
-		framesBuf.add(buffer);
-		byte[] src = framesBuf.elements(seekPos, framesBuf.size() - seekPos);
-
-		//NALセパレーターの位置
-		List<Integer> delimiters = ByteUtil.kmp(src, NAL_DELIMITER);
-
-		if (DEBUG) {
-			StringBuilder sb = new StringBuilder();
-			for (Integer delimiter : delimiters) {
-				sb.append(delimiter);
-				sb.append(", ");
-			}
-			//Log.d("AAA", "delimiters: " + sb.toString() + " src: " + src.length);
-		}
-		
-		for (int i = 0; i < delimiters.size(); i++) {
-			
-			//次のフレームのフレームタイプを取得
-			if (delimiters.get(i) + NAL_DELIMITER.length < src.length) {
-				nextNalType = src[delimiters.get(i) + NAL_DELIMITER.length];
-			} else {
-				break;
-			}
-
-			//現在のフルフレームの終了位置を取得
-			int endPos = i==0 ? seekPos + delimiters.get(0) : delimiters.get(i) - delimiters.get(i-1) + (isNalDelimiter4 ? 1: 0);
-			
-			//セパレーターが 0x00 00 00 01 であるかどうかを決定
-			isNalDelimiter4 = ( endPos != 0 && i== 0 && delimiters.get(i) != 0 && src[delimiters.get(i)-1] == 0x00 );
-			endPos = isNalDelimiter4 ? endPos - 1 : endPos;
-			
-			if (waitingIDRFrame) {
-				//次のフレームがIDRかどうかを判別
-				if (isIDRFrame(nextNalType)) {
-					waitingIDRFrame = false;
-				} else {
-					//Log.d("AAA", "Remove incomplete frame data.");
-					//IDRフレームの前に不完全なフレームデータを削除する
-					framesBuf.remove(0, endPos);
-					continue;
-				}
-			}
-
-			if (currentNalType == H264NT_SLICE || currentNalType == H264NT_SLICE_IDR) {
-				
-				//完全なフレームデータを取得する（IDRフレームと、IDRフレームの前のSPS + PPSが全体である場合）
-				byte[] avcBuf = framesBuf.remove(0, endPos);
-				
-				if (avcBuf.length > NAL_DELIMITER.length) {
-					boolean isLastFrame = (nextNalType & 0x1F) == H264NT_SPS;
-					
-					int frameType = H264NalUtil.getPesFrameType(avcBuf);
-					List<AvcFrame> encodeAvcFrames = getEncodeAvcFrames( new AvcFrame(avcBuf, frameType, pts, -1), isLastFrame);
-					numInGop++;
-					for (AvcFrame avcFrame : encodeAvcFrames) {
-						tsWriter.writeH264(isFirstPes, avcFrame.payload, avcFrame.payload.length, avcFrame.pts, avcFrame.dts);
-						isFirstPes = false;
-					}
-					
-					tsSegTime = numInGop / this.fps;
-					if (isLastFrame && tsSegTime >= 10F) {
-						waitingIDRFrame = true;
-						isFirstPes = true;
-						tsSegTime = numInGop / this.fps;
-						prepare4NextTs();
-					}
-				}
-				
-			}
-
-			//更新currentNalType
-			if (((nextNalType & 0x1F) == H264NT_SLICE_IDR) || ((nextNalType & 0x1F) == H264NT_SLICE)) {
-				currentNalType = nextNalType & 0x1F;
-			}
-			
-			if (waitingIDRFrame) {
-				currentNalType = 0;
-			}
-		}
-		
-		return null;
-	}
-	
-	//mixed itf
-	public AvcResult process(byte[] rawData) {
-
-		boolean isNalDelimiter4 = false;
-		byte nextNalType = H264NT_UNUSED_TYPE;
-		
-		int seekPos =  framesBuf.size() >= NAL_DELIMITER.length ? framesBuf.size() - NAL_DELIMITER.length : 0;
-		
-		framesBuf.add(rawData);
-		byte[] src = framesBuf.elements(seekPos, framesBuf.size() - seekPos);
-		
-		List<Integer> delimiters = ByteUtil.kmp(src, NAL_DELIMITER);
-		List<AvcFrame> encodeAvcFrames = new ArrayList<AvcFrame>();
-		List<AvcFrame> endAvcFrames = new ArrayList<AvcFrame>();
-		encodeAvcFrames.addAll(cacheAvcFrames);
-		cacheAvcFrames.clear();
-		boolean isTailAvc = false;
-		
-		for(int i = 0; i < delimiters.size(); i++) {
-			if(delimiters.get(i) + NAL_DELIMITER.length < src.length)
-				nextNalType = src[delimiters.get(i) + NAL_DELIMITER.length];
-			else 
-				break;
-			
-			int endPos = i==0 ? seekPos + delimiters.get(0) : delimiters.get(i) - delimiters.get(i-1) + (isNalDelimiter4 ? 1: 0);
-			
-			isNalDelimiter4 = ( endPos != 0 && i== 0 && delimiters.get(i) != 0 && src[delimiters.get(i)-1] == 0x00 );
-			endPos = isNalDelimiter4 ? endPos -1 : endPos;
-			
-			if(waitingIDRFrame) {
-				if(isIDRFrame(nextNalType))
-					waitingIDRFrame = false;
-				else {
-					framesBuf.remove(0, endPos);
-					continue;
-				}
-			}
-			
-			if(currentNalType == H264NT_SLICE || currentNalType == H264NT_SLICE_IDR) {
-				
-				byte[] avcBuf = framesBuf.remove(0, endPos);
-				
-				if(avcBuf != null && avcBuf.length > NAL_DELIMITER.length) {
-					
-					boolean isLastFrame = (nextNalType & 0x1F) == H264NT_SPS;
-					int frameType = H264NalUtil.getPesFrameType(avcBuf);
-					
-					encodeAvcFrames.addAll(getEncodeAvcFrames( new AvcFrame(avcBuf, frameType, -1, -1), isLastFrame));
-					if(!isTailAvc && isLastFrame) {
-						endAvcFrames.addAll(encodeAvcFrames);
-						encodeAvcFrames.clear();
-						isTailAvc = true;
-						waitingIDRFrame = true;
-					}
-					
-				}
-			}
-			if (((nextNalType & 0x1F) == H264NT_SLICE_IDR) || ((nextNalType & 0x1F) == H264NT_SLICE)) {
-				currentNalType = nextNalType & 0x1F;
-			}
-			
-			if (waitingIDRFrame)
-				currentNalType = 0;
-		}
-		return endAvcFrames.isEmpty() && encodeAvcFrames.isEmpty() ? null : new AvcResult(isTailAvc ? endAvcFrames : encodeAvcFrames, isTailAvc);
 	}
 	
 	@Override
 	public void close() {
 	}
+
 	
-	private List<AvcFrame> getEncodeAvcFrames(AvcFrame avcFrame, boolean isLastFrame) {
-		List<AvcFrame> avcFrames = new ArrayList<AvcFrame>();
-		
-		switch(avcFrame.frameType) {
-		case FRAME_I:
-		case FRAME_P:
-		case UNSUPPORT:
-			if(!avcFrameCache.isEmpty()) {
-				AvcFrame avcFrame2 = avcFrameCache.pop();
-				avcFrame2.pts = getPts();
-				avcFrame2.dts = avcFrame2.pts;
-				avcFrames.add(avcFrame2);
-				while(!avcFrameCache.isEmpty())
-					avcFrames.add(avcFrameCache.pop());
-			}
-			break;
-			
-		case FRAME_B:
-			avcFrame.pts = getPts();
-			avcFrame.dts = avcFrame.pts;
-			break;
-		}
-		
-		avcFrameCache.offer(avcFrame);
-		
-		if(isLastFrame) {
-			
-			AvcFrame avcFrame2 = avcFrameCache.pop();
-			avcFrame2.pts = getPts();
-			avcFrame2.dts = avcFrame2.pts;
-			avcFrames.add(avcFrame2);
-			while(!avcFrameCache.isEmpty())
-				avcFrames.add(avcFrameCache.pop());
-		}
-		
-		return avcFrames;
-	}
-	
-	private synchronized long getPts() {
-		//return (((System.currentTimeMillis()) * 90) % 8589934592L);
+	private long getPts() {
 		return pts += ptsIncPerFrame;
 	}
 	
 	private long getDts() {
 		return dts += ptsIncPerFrame;
-	}
-	
-	private boolean isIDRFrame(byte nalType) {
-		return (nalType & 0x1F) == H264NT_SPS ||(nalType & 0x1F) == H264NT_PPS || (nalType & 0x1F) == H264NT_SLICE_IDR;
-	}
-	
-	static class AvcResult {
-		
-		List<TsPacketWriter.FrameData> avcFrames = new ArrayList<TsPacketWriter.FrameData>();
-		boolean isTailAvc;
-		
-		AvcResult(List<AvcFrame> avcFrames, boolean isTailAvc) {
-			for(AvcFrame frame : avcFrames) {
-				TsPacketWriter.FrameData frameData = new TsPacketWriter.FrameData();
-				frameData.buf = frame.payload;
-				frameData.pts = frame.pts;
-				frameData.dts = frame.dts;
-				frameData.isAudio = false;
-				this.avcFrames.add(frameData);
-			}
-			this.isTailAvc = isTailAvc;
-		}
 	}
 	
 	static class AvcFrame {

0dd4d31bba79756812f6ac5831d2cbf2a95fd275
Masaru Takano <mtaka@gclue.jp>
2020-08-02 16:47:01 +0900

    System プロファイルで開く設定画面を変更。

--- dConnectDevicePlugin/dConnectDeviceMidi/plugin/src/main/java/org/deviceconnect/android/deviceplugin/midi/profiles/MidiSystemProfile.java
+++ dConnectDevicePlugin/dConnectDeviceMidi/plugin/src/main/java/org/deviceconnect/android/deviceplugin/midi/profiles/MidiSystemProfile.java
@@ -4,13 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import org.deviceconnect.android.deviceplugin.midi.DConnectMidiServiceListActivity;
+import org.deviceconnect.android.deviceplugin.midi.DConnectMidiSettingsListActivity;
 import org.deviceconnect.android.profile.SystemProfile;
 
 
 public class MidiSystemProfile extends SystemProfile {
     @Override
     protected Class<? extends Activity> getSettingPageActivity(final Intent request, final Bundle param) {
-        return DConnectMidiServiceListActivity.class;
+        return DConnectMidiSettingsListActivity.class;
     }
 }

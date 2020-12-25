bb0300e23bbd23a9141be0a40e6103842753e87b
小林伸郎 <nobu@gclue.jp>
2020-12-25 18:08:28 +0900

    エラー文言を string.xml に追加

--- dConnectDevicePlugin/dConnectDeviceHost/app/src/main/java/org/deviceconnect/android/deviceplugin/host/activity/fragment/PermissionErrorDialogFragment.java
+++ dConnectDevicePlugin/dConnectDeviceHost/app/src/main/java/org/deviceconnect/android/deviceplugin/host/activity/fragment/PermissionErrorDialogFragment.java
@@ -7,13 +7,15 @@ import android.os.Bundle;
 
 import androidx.fragment.app.DialogFragment;
 
+import org.deviceconnect.android.deviceplugin.host.R;
+
 public class PermissionErrorDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
-        builder.setTitle("パーミッションエラー");
-        builder.setMessage("カメラ映像を配信するためのパーミッションが許可されなかったので、アプリケーションを終了します。");
-        builder.setPositiveButton("はい", (dialog, which) -> {
+        builder.setTitle(R.string.host_error_permission_title);
+        builder.setMessage(R.string.host_error_permission_message);
+        builder.setPositiveButton(R.string.host_error_positive, (dialog, which) -> {
             Activity a = getActivity();
             if (a != null) {
                 a.finish();

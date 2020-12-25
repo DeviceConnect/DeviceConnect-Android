4d4be5822c88a6caaa5da65c97ecb339a936e438
小林伸郎 <nobu@gclue.jp>
2020-12-25 17:48:57 +0900

    パッケージ名を変更したので削除します。

--- dConnectDevicePlugin/dConnectDeviceHost/app/src/main/java/org/deviceconnect/android/deviceplugin/host/activity/profile/HostProfileActivity.java
+++
@@ -1,6 +0,0 @@
-package org.deviceconnect.android.deviceplugin.host.activity.profile;
-
-import org.deviceconnect.android.deviceplugin.host.activity.HostDevicePluginBindActivity;
-
-public class HostProfileActivity extends HostDevicePluginBindActivity {
-}

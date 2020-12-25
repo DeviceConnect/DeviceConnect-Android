2219824b2cf3c3054c78a460b9ddad7fe67c6f34
小林伸郎 <nobu@gclue.jp>
2020-12-25 18:26:22 +0900

    Managerに接続中の画面を修正

--- dConnectDevicePlugin/dConnectDeviceHost/app/src/main/java/org/deviceconnect/android/deviceplugin/host/activity/fragment/ManagerStartingConfirmationFragment.java
+++ dConnectDevicePlugin/dConnectDeviceHost/app/src/main/java/org/deviceconnect/android/deviceplugin/host/activity/fragment/ManagerStartingConfirmationFragment.java
@@ -21,7 +21,7 @@ public abstract class ManagerStartingConfirmationFragment extends HostDevicePlug
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_host_camera_check_manager, container, false);
+        return inflater.inflate(R.layout.fragment_host_manager_starting_confirmation, container, false);
     }
 
     @Override

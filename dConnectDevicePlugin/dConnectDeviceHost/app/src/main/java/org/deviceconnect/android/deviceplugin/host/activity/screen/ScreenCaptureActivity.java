0b372f666c3c3cd8c2868a883ae3f382d581d5a2
小林伸郎 <nobu@gclue.jp>
2020-12-25 14:39:50 +0900

    スクリーンキャストアプリに戻るボタンを追加

--- dConnectDevicePlugin/dConnectDeviceHost/app/src/main/java/org/deviceconnect/android/deviceplugin/host/activity/screen/ScreenCaptureActivity.java
+++ dConnectDevicePlugin/dConnectDeviceHost/app/src/main/java/org/deviceconnect/android/deviceplugin/host/activity/screen/ScreenCaptureActivity.java
@@ -1,6 +1,9 @@
 package org.deviceconnect.android.deviceplugin.host.activity.screen;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBar;
 
 import org.deviceconnect.android.deviceplugin.host.R;
 import org.deviceconnect.android.deviceplugin.host.activity.HostDevicePluginBindActivity;
@@ -10,5 +13,19 @@ public class ScreenCaptureActivity extends HostDevicePluginBindActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_capture);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
